0b4b01d94706b0918414ff04173afa2cc5151de1
Byiringiro <byives21@gmail.com>
2019-02-23 18:08:51 +0000

    last commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
     belongs_to :user
 	
-    has_attached_file :image, styles: { medium: "400x600#"}, validate_media_type: false
+    has_attached_file :image, styles: { medium: "350x350#"}, validate_media_type: false
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
